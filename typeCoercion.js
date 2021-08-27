982c090fd24dd86972f6c6589dd633a4a7ef95e4
isaaccavallaro <isaaccavallaro@gmail.com>
2021-08-27 12:14:13 +1000

    adding type coercion code

---
+++ typeCoercion.js
@@ -0,0 +1,12 @@
+////////// + operator ////////////
+
+// console.log(20 + 20)
+// console.log(20 + '20')
+// console.log(1 + "2")
+// console.log(1 + "ab")
+
+// console.log(1 + 2 + "1")
+// console.log(1 + 2 + "2")
+// console.log(2 + 5 + "a")
+
+////////// - operator ////////////
