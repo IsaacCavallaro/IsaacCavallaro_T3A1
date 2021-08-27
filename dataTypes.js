6b289488dd73fbff03823a432f2e36db13883cc9
isaaccavallaro <isaaccavallaro@gmail.com>
2021-08-27 12:21:17 +1000

    adding datatypes code

---
+++ dataTypes.js
@@ -0,0 +1,12 @@
+////////--number--/////////
+// console.log(typeof 1)
+// console.log(typeof 100)
+
+
+////////--string--/////////
+// console.log(typeof "")
+// console.log(typeof "123")
+
+////////--boolean--/////////
+// console.log(typeof true)
+// console.log(typeof false)
